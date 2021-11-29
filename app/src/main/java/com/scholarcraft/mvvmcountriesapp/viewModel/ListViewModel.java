5843e72ac81ef9f9351926962a920bb959471aa4
TulioPerez <saefAllah@gmail.com>
2021-11-29 11:23:29 -0500

    Base model, view, viewModel classes created

--- app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
+++ app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
@@ -1,4 +1,35 @@
 package com.scholarcraft.mvvmcountriesapp.viewModel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.scholarcraft.mvvmcountriesapp.model.CountryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewModel extends ViewModel {
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
+    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+
+    public void refresh() {
+        fetchCountries();
+    }
+
+    private void fetchCountries() {
+        //temp values will be changed after api integration... this is mock data
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Spain", "Madrid", "");
+        CountryModel country3 = new CountryModel("New Mexico", "Mexico City", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        countryLoadError.setValue(false);
+        loading.setValue(false);
+    }
 }
