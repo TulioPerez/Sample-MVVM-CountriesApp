37c1e8626b63b4eed1f0643e4f707687d1735c83
TulioPerez <saefAllah@gmail.com>
2021-11-29 15:17:56 -0500

    Framework completed with mock data; no country flags
    Remaining:
    Countries API integration

--- app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
+++ app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
@@ -20,13 +20,33 @@ public class ListViewModel extends ViewModel {
     private void fetchCountries() {
         //temp values will be changed after api integration... this is mock data
         CountryModel country1 = new CountryModel("Albania", "Tirana", "");
-        CountryModel country2 = new CountryModel("Spain", "Madrid", "");
-        CountryModel country3 = new CountryModel("New Mexico", "Mexico City", "");
+        CountryModel country2 = new CountryModel("Algeria", "Algiers", "");
+        CountryModel country3 = new CountryModel("Australia", "Canberra", "");
+        CountryModel country4 = new CountryModel("Brazil", "Brazilia", "");
+        CountryModel country5 = new CountryModel("Egypt", "Cairo", "");
+        CountryModel country6 = new CountryModel("Italy", "Rome", "");
+        CountryModel country7 = new CountryModel("Lebanon", "Beirut", "");
+        CountryModel country8 = new CountryModel("New Mexico", "Mexico City", "");
+        CountryModel country9 = new CountryModel("Saudi Arabia", "Riyadh", "");
+        CountryModel country10 = new CountryModel("South Africa", "Cape Town", "");
+        CountryModel country11 = new CountryModel("Spain", "Madrid", "");
+        CountryModel country12 = new CountryModel("UAE", "Abu Dhabi", "");
+        CountryModel country13 = new CountryModel("USA", "Washington D.C.", "");
 
         List<CountryModel> list = new ArrayList<>();
         list.add(country1);
         list.add(country2);
         list.add(country3);
+        list.add(country4);
+        list.add(country5);
+        list.add(country6);
+        list.add(country7);
+        list.add(country8);
+        list.add(country9);
+        list.add(country10);
+        list.add(country11);
+        list.add(country12);
+        list.add(country13);
 
         countries.setValue(list);
         countryLoadError.setValue(false);
