da0c6e4c54672565c60689d5ae2d3c9d6988d1e4
Tulio Perez <tulio.p@vibenomics.com>
2022-05-08 11:14:16 -0400

    This is a test

--- app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
+++ app/src/main/java/com/scholarcraft/mvvmcountriesapp/viewModel/ListViewModel.java
@@ -3,26 +3,59 @@ package com.scholarcraft.mvvmcountriesapp.viewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.scholarcraft.mvvmcountriesapp.model.CountriesService;
 import com.scholarcraft.mvvmcountriesapp.model.CountryModel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ListViewModel extends ViewModel {
     public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
     public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
 
+    private CountriesService countriesService = CountriesService.getInstance();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+
     public void refresh() {
         fetchCountries();
     }
 
     private void fetchCountries() {
+        loading.setValue(true);
+        disposable.add(
+                countriesService.getCountries()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<CountryModel>>() {
 
+                            @Override
+                            public void onSuccess(@NonNull List<CountryModel> countryModels) {
 
+                            }
 
+                            @Override
+                            public void onError(@NonNull Throwable e) {
 
-//        // mock data used for testing, prior to api integration
+                            }
+                        })
+        );
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
+    }
+
+    //        // mock data used for testing, prior to api integration
 //        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
 //        CountryModel country2 = new CountryModel("Algeria", "Algiers", "");
 //        CountryModel country3 = new CountryModel("Australia", "Canberra", "");
@@ -55,5 +88,4 @@ public class ListViewModel extends ViewModel {
 //        countries.setValue(list);
 //        countryLoadError.setValue(false);
 //        loading.setValue(false);
-    }
 }
